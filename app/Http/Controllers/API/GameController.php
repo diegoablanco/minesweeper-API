002f71745494864cb020ecb8c0245fc84ead9de7
Diego <diegoablanco@gmail.com>
2020-01-31 13:10:04 -0300

    move to api folder

--- app/Http/Controllers/API/GameController.php
+++ app/Http/Controllers/API/GameController.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Repositories\GameRepository;
+use App\Http\Controllers\Controller;
 
 class GameController extends Controller
 {
+    public function __construct(GameRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index()
     {
         return response()->json(null, 405);
@@ -18,7 +25,8 @@ class GameController extends Controller
 
     public function create(Request $request)
     {
-        return response()->json(null, 405);
+        $game = $this->repository->create($request->all());
+        return response()->json($game, 201);
     }
 
     public function flag(Request $request, $cell)
