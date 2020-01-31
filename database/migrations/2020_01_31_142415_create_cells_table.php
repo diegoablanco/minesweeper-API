5528ae7215880204690127bde3e7ba3710fe8445
Diego <diegoablanco@gmail.com>
2020-01-31 11:49:41 -0300

    add migrations

---
+++ database/migrations/2020_01_31_142415_create_cells_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCellsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cells', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('row_id');
+            $table->string('state')->default('unrev');
+	        $table->boolean('is_bomb')->default(0);
+            $table->unsignedInteger('neighbouring_bombs')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cells');
+    }
+}
